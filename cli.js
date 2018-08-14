0877f11167fa88478d54b2c8a4470eebefaac86c
Sebastian Speitel <sebastian.speitel@outlook.de>
2018-08-14 14:33:20 +0200

    really moved source files

--- cli.js
+++
@@ -1,8 +0,0 @@
-﻿#!/usr/bin/env node
-const chalk = require('chalk');
-const { Block } = require('./blockchain.js');
-
-console.log(chalk.red(255..toString(16)));
-
-console.log(chalk.blue("test"));
-//console.log(process.argv);
